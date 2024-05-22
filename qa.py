a966090f59828ba64e0f0b2b0bcda69ac91c43e5
chien.uet <82660465+chungocchien@users.noreply.github.com>
2024-05-22 09:27:33 +0700

    Update qa.py

--- qa.py
+++ qa.py
@@ -1,6 +1,7 @@
 
 from langchain.chains import LLMChain, RetrievalQA
 from langchain_cohere import CohereEmbeddings
+from langchain_cohere import ChatCohere
 from langchain_community.llms import CTransformers, Ollama, LlamaCpp
 from langchain.prompts import PromptTemplate
 from langchain_community.vectorstores import FAISS
@@ -9,7 +10,7 @@ import streamlit as st
 
 def read_vectordb():
     embeddings = CohereEmbeddings(model="embed-multilingual-v3.0",
-                                  cohere_api_key='jgfGLUxBFOGVtZzQyNLcorveyREv7i8ENFRTQADk')
+                                  cohere_api_key='u1KIZqTOxVjnXCaYzePcivgr0qKxQtmC3gfedKME')
     db = FAISS.load_local(vector_db_path, embeddings)
     return db
 def close_docs(question, db):
@@ -19,26 +20,28 @@ def close_docs(question, db):
 def create_prompt(template):
     prompt= PromptTemplate(template=template, input_variables=["context", "question"])
     return prompt
-def load_file(model_file):
-    llm = LlamaCpp( model_path=model_file,
-                    n_ctx=2048,
-                    temperature=0,
-                    max_tokens=512,
-                    verbose=True,
-    )
+def load_file():
+    # llm = LlamaCpp( model_path=model_file,
+    #                 n_ctx=2048,
+    #                 temperature=0,
+    #                 max_tokens=512,
+    #                 verbose=True,
+    # )
+    llm = ChatCohere(model="command-r", cohere_api_key='u1KIZqTOxVjnXCaYzePcivgr0qKxQtmC3gfedKME')
+
     return llm
 
 def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
+    return "\n".join(doc.page_content for doc in docs)
 
 model_file = "models/vinallama-7b-chat_q5_0.gguf"
 vector_db_path = "vectorstores/db_faiss"
 
 db = read_vectordb()
-llm = load_file(model_file)
+llm = load_file()
 
 template = """<|im_start|>system\nSử dụng thông tin sau đây để trả lời câu hỏi.
-Nếu bạn không biết câu trả lời, hãy nói không biết, đừng cố tạo ra câu trả lời\n
+Nếu bạn không biết câu trả lời, hãy trả lời "Tôi không biết" \n
     {context}<|im_end|>\n<|im_start|>user\n{question}<|im_end|>\n<|im_start|>assistant"""
 prompt = create_prompt(template)
 llm_chain = LLMChain(llm=llm, prompt=prompt)
@@ -47,14 +50,14 @@ llm_chain = LLMChain(llm=llm, prompt=prompt)
 
 st.set_page_config(page_title="Q&A Demo")
 
-st.header("Gemini LLM Application")
+st.header("Giải đáp Luật lao động Việt Nam ")
 
 # Initialize session state for chat history if it doesn't exist
 if 'chat_history' not in st.session_state:
     st.session_state['chat_history'] = []
 
-question = st.text_input("Input: ", key="input")
-submit = st.button("Ask the question")
+question = st.text_input("Câu hỏi: ", key="input")
+submit = st.button("Đặt câu hỏi")
 
 if submit and question:
     docs = close_docs(question, db)
@@ -62,12 +65,12 @@ if submit and question:
     print(context)
     response = llm_chain.run({"context":context,"question": question})
     # Add user query and response to session state chat history
-    st.session_state['chat_history'].append(("You", question))
-    st.subheader("The Response is")
+    st.session_state['chat_history'].append(("Bạn", question))
+    st.subheader("Câu trả lời:")
     # for chunk in response:
     st.write(response)
     st.session_state['chat_history'].append(("Bot", response))
-st.subheader("The Chat History is")
+st.subheader("Lịch sử trò chuyện")
 
 for role, text in st.session_state['chat_history']:
     st.write(f"{role}: {text}")
